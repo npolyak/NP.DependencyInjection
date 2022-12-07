d7a576c3a261d274f1a2889bd4e74f3179ba3617
npolyak <nick.polyak@verizon.net>
2022-12-07 13:07:36 -0500

    added comments

--- src/Interfaces/IContainerBuilder.cs
+++ src/Interfaces/IContainerBuilder.cs
@@ -17,75 +17,114 @@ namespace NP.DependencyInjection.Interfaces
 {
     public interface IContainerBuilder
     {
+        // register a non-singleton cell producing objects of type 'typeToResolve'
+        // to be composed and returned by (resolvingType, resolutionKey) pair. 
         void RegisterType(Type resolvingType, Type typeToResolve, object? resolutionKey = null);
 
+        // register a non-singleton cell producing objects of type 'TToResolve'
+        // to be composed and returned by (typeof(TResolving), resolutionKey) pair. 
         void RegisterType<TResolving, TToResolve>(object? resolutionKey = null)
             where TToResolve : TResolving;
 
+        // register a singleton cell returning the object instance
+        // by (resolvingType, resolutionKey) pair.
         void RegisterSingletonInstance
         (
             Type resolvingType,
             object instance,
             object? resolutionKey = null);
 
+        // register a singleton cell returning the object instance
+        // by (typeof(TResolving), resolutionKey) pair.
         void RegisterSingletonInstance<TResolving>(object instance, object? resolutionKey = null);
 
+        // register a singleton cell producing objects of type 'typeToResolve'
+        // to be composed and returned by (resolvingType, resolutionKey) pair. 
         void RegisterSingletonType<TResolving, TToResolve>(object? resolutionKey = null)
              where TToResolve : TResolving;
 
+        // register a singleton cell using resolvingFunc to produce
+        // an object to be returned by (typeof(TResolving), resolutionKey) pair
         void RegisterSingletonFactoryMethod<TResolving>(
             Func<TResolving> resolvingFunc,
             object? resolutionKey = null
         );
 
+
+        // register a non-singleton cell using resolvingFunc to produce
+        // an object to be returned by (typeof(TResolving), resolutionKey) pair
         void RegisterFactoryMethod<TResolving>
         (
             Func<TResolving> resolvingFunc,
             object? resolutionKey = null
         );
 
+
+        // register a singleton cell using resolving method given by its MethodInfo to produce
+        // an object to be returned by (resolvingType, resolutionKey) pair
         void RegisterSingletonFactoryMethodInfo
         (
             MethodInfo factoryMethodInfo,
             Type? resolvingType = null,
             object? resolutionKey = null);
 
+        // register a singleton cell using resolving method given by its MethodInfo to produce
+        // an object to be returned by (typeof(TToResolve), resolutionKey) pair
         void RegisterSingletonFactoryMethodInfo<TToResolve>
         (
             MethodInfo factoryMethodInfo,
             object? resolutionKey = null);
 
-
+        // register a non-singleton cell using resolving method given by its MethodInfo to produce
+        // an object to be returned by (resolvingType, resolutionKey) pair
         void RegisterFactoryMethodInfo
         (
             MethodInfo factoryMethodInfo,
             Type? resolvingType = null,
             object? resolutionKey = null);
 
+
+        // register a non-singleton cell using resolving method given by its MethodInfo to produce
+        // an object to be returned by (typeof(TResolving), resolutionKey) pair
         public void RegisterFactoryMethodInfo<TResolving>
         (
             MethodInfo factoryMethodInfo,
             object? resolutionKey = null);
 
+        // register a type that has RegisterType class level attribute. This attribute
+        // should provide the resolvingType (if not - resolving type is
+        // assumed to be the one from which the current type is derived),
+        // also resolutionKey (if needed) and flag indication whether it is a singleton or not
+        // (by default it is not a singleton)
         void RegisterAttributedType(Type resolvingType);
 
+        // check every public type within the assembly and register it if it has
+        // RegisterTypeAttribute
         void RegisterAssembly(Assembly assembly);
 
+        // Load the assembly and register all its public types that have 
+        // RegisterTypeAttribute
         void RegisterDynamicAssemblyByFullPath(string assemblyPath);
 
+        // Load and register all assemblies from the folder that match
+        // the regex
         void RegisterPluginsFromFolder
         (
             string assemblyFolderPath,
             Regex? matchingFileName = null
         );
 
+        // load and register all assemblies that match 
+        // the regex from all the subfolders of the base folder. 
         void RegisterPluginsFromSubFolders
         (
             string baseFolderPath,
             Regex? matchingFileName = null);
 
+        // unregister a cell by (resolvingType, resolutionKey) pair. 
         void UnRegister(Type resolvingType, object? resolutionKey = null);
 
+        // create the unmodifyable DI container
         IDependencyInjectionContainer Build();
     }
 }
--- src/Interfaces/IDependencyInjectionContainer.cs
+++ src/Interfaces/IDependencyInjectionContainer.cs
@@ -11,14 +11,19 @@
 
 namespace NP.DependencyInjection.Interfaces
 {
+    // unmodifyable IoC (DI) container that returns composed object
     public interface IDependencyInjectionContainer
     {
+        // composes all the properties marked with InjectAttribute 
+        // for the object
         void ComposeObject(object obj);
 
-        object Resolve(Type typeToResolve, object? resolutionKey = null);
+        // returns (and if appropriate also composes) an object
+        // corresponding to (resolvingType, resolutionKey) pair
+        object Resolve(Type resolvingType, object? resolutionKey = null);
 
-        TToResolve Resolve<TToResolve>(object? resolutionKey = null);
-
-
+        // returns (and if appropriate also composes) an object
+        // corresponding to (typeof(TResolving), resolutionKey) pair
+        TResolving Resolve<TResolving>(object? resolutionKey = null);
     }
 }
